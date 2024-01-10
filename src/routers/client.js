17c627bcb96d047fb70f4b9ef48040191ac72c81
Vini0667 <vini04.silva09+github@gmail.com>
2024-01-10 00:24:31 -0400

    Customers registration completed

--- src/routers/client.js
+++ src/routers/client.js
@@ -1,15 +1,36 @@
 // Requiring packages
-    const express = require("express");
+    const express = require(`express`);
 
 // Requiring my files
+    const { registerClientDataValidation } = require(`../helpers/helper_cliente`);
+    const Client = require(`../models/Client`);
 
 // Defining variables
     const router = express.Router();
 
 // Routes
 
-// Get routes
+    // Get routes
+        router.get(`/register`, (req, res) => {
+            res.render(`client/register-client`, {
+                title: `Cadastrar Cliente`
+            });
+        });
 
-// Post routes
+    // Post routes
+        router.post(`/register`, registerClientDataValidation, (req, res) => {
+            let newClient = {
+                name: req.body.name,
+                contact: req.body.contact
+            };
+
+            new Client(newClient).save().then(() => {
+                req.flash(`success_msg`, `O cadastro de ${req.body.name} foi um sucesso!`);
+                res.redirect("/");
+            }).catch((error) => {
+                req.flash(`error_msg`, `Ouve um erro interno no cadastramento. ERRO: ${error}. Tente novamente mais tarde`);
+                res.redirect("/");
+            });
+        });
 
 module.exports = router;
